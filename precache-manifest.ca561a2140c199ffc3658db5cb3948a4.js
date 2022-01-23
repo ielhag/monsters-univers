e7400515b75b7337176841239021f1919b2a1fe5
Ibrahim Elhag <i@elhags.com>
2022-01-23 13:25:19 +0300

    Updates

---
+++ precache-manifest.ca561a2140c199ffc3658db5cb3948a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52701aacb02df736c2ba9e498e2d297b",
+    "url": "/monsters-univers/index.html"
+  },
+  {
+    "revision": "d6595040c33ed0719ce2",
+    "url": "/monsters-univers/static/css/main.b7ed144d.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/monsters-univers/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/monsters-univers/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "d6595040c33ed0719ce2",
+    "url": "/monsters-univers/static/js/main.ba4a9154.chunk.js"
+  },
+  {
+    "revision": "89c93d7e53320cbb0f49",
+    "url": "/monsters-univers/static/js/runtime~main.aaae4706.js"
+  }
+]);
